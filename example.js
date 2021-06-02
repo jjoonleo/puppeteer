53e00a932e8349ca405db86844cf5c0d3575898c
Ejun <67417817+jjoonleo@users.noreply.github.com>
2021-06-02 09:16:39 +0900

    change school into json

--- example.js
+++ example.js
@@ -2,16 +2,50 @@ const puppeteer = require('puppeteer-core');
 
 var members = [
    {
+      "city":"서울특별시",
+      "schoolLevel":"고등학교",
+      "shchoolName":"선덕고등학교",
       "name": "박이준",
       "birthday": "050728",
       "password": "6433",
    },
    {
+      "city":"서울특별시",
+      "schoolLevel":"고등학교",
+      "shchoolName":"선덕고등학교",
       "name": "전무진",
       "birthday": "050616",
       "password": "1004",
    }
 ];
+var shchoolLevelNumber = {
+   "유치원":"1",
+   "초등학교":"2",
+   "중학교":"3",
+   "고등학교":"4",
+   "특수학교 등":"5",
+}
+var cityNumber = {
+   "서울특별시":"01",
+   "부산광역시":"02",
+   "대구광역시":"03",
+   "인천광역시":"04",
+   "광주광역시":"05",
+   "대전광역시":"06",
+   "울산광역시":"07",
+   "세종특별자치시":"08",
+   "경기도":"09",
+   "강원도":"11",
+   "충청북도":"12",
+   "충청남도":"13",
+   "전라북도":"14",
+   "전라남도":"15",
+   "경상북도":"16",
+   "경상남도":"17",
+   "제주특별자치도":"18"
+
+}
+
 
 console.log(members[0]['name']);
 for(let i = 0; i < 2; i++){
@@ -22,8 +56,8 @@ for(let i = 0; i < 2; i++){
 
   await page.click('#btnConfirm2');
   await page.click('#WriteInfoForm > table > tbody > tr:nth-child(1) > td > button');
-  await page.select('#sidolabel', "01");
-  await page.select('#crseScCode', "4");
+  await page.select('#sidolabel', cityNumber[members[i]['shchoolName']]);
+  await page.select('#crseScCode', shchoolLevelNumber[members[i]['shchoolLevel']]);
   await page.type('#orgname',"선덕고",{ delay: 100 });
   await page.click('#softBoardListLayer > div.layerContentsWrap > div.layerSchoolSelectWrap > table > tbody > tr:nth-child(3) > td:nth-child(3) > button');
   await page.mouse.move(200, 500);await page.waitForTimeout(100);
